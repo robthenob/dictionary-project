dd47e3578834059ee05df1f44e625f21b3d6f492
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-15 19:54:49 -0700

    implemented word and meaning results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`WTF is a ${keyword}??!!`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -28,6 +29,7 @@ export default function Dictionary() {
           autoFocus={true}
           onChange={handleKeywordChange}
         />
+        <Results results={results} />
       </form>
     </div>
   );
