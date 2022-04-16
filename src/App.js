497ae055f5123b1233addb77c9a7c12e5b748b10
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-15 18:07:40 -0700

    added open source footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
       <header className="App-header">
         <h1>Dictionary App</h1>
       </header>
+      <footer>
+        This dictionary application was coded by Randi Bradley. It is open
+        sourced on{" "}
+        <a href="https://github.com/robthenob/dictionary-project">Github</a> and
+        hosted on{" "}
+        <a href="https://sensational-snickerdoodle-a846ea.netlify.app">
+          Netlify
+        </a>{" "}
+      </footer>
     </div>
   );
 }
