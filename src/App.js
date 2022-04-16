ebe6fa1da32c904931cae29b1c4c21f9bc0efcf2
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-15 18:35:42 -0700

    implemented search bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-      </header>
-      <footer>
-        This dictionary application was coded by Randi Bradley. It is open
-        sourced on{" "}
-        <a href="https://github.com/robthenob/dictionary-project">Github</a> and
-        hosted on{" "}
-        <a href="https://sensational-snickerdoodle-a846ea.netlify.app">
-          Netlify
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This dictionary application was coded by Randi Bradley. It is open
+          sourced on{" "}
+          <a href="https://github.com/robthenob/dictionary-project">Github</a>{" "}
+          and hosted on{" "}
+          <a href="https://sensational-snickerdoodle-a846ea.netlify.app">
+            Netlify
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
