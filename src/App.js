2415ca5d841c064f4e7248757a97fa106ff4b367
robthenob <92840644+robthenob@users.noreply.github.com>
2022-04-18 14:35:41 -0700

    final touches
    added new tab icon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Dictionary</h1>
+          <h1>Dictionary App</h1>
         </header>
         <main>
           <Dictionary />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -34,7 +34,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <section>
-        <h2>What word do you want to look up?</h2>
+        <h2>What word do you want to look up?🔎</h2>
         <form onSubmit={search}>
           <input
             type="search"
